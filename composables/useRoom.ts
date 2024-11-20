d6d132eb8328246b92576c70c4483fc37f2b4060
dragon <peterlife0617@gmail.com>
2024-11-20 21:40:11 +0800

    feat(day08): add dynamic route and 404 page

--- composables/useRoom.ts
+++ composables/useRoom.ts
@@ -1,5 +1,5 @@
 export const useRoom = () => {
-  const roomList  = ref([]);
+  const roomList = ref([]);
   const isLoading = ref(false);
 
   const getRooms = async () => {
@@ -14,9 +14,26 @@ export const useRoom = () => {
 
   }
 
+  const getRoom = async (id: string) => {
+    isLoading.value = true;
+
+    try {
+      if (!id) {
+        return {};
+      }
+      const newsResponse = await fetch(`https://nuxr3.zeabur.app/api/v1/rooms/${id}`).then((res) => res.json())
+      return newsResponse.result;
+    } finally {
+      isLoading.value = false;
+    }
+
+  }
+
+
   return {
-    roomList ,
+    roomList,
     isLoading,
     getRooms,
+    getRoom
   }
 }
