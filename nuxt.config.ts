5c75f7c12b09dbbf37acf9de4a2fa72de19037e7
dragon <peterlife0617@gmail.com>
2024-11-20 21:40:14 +0800

    chore: add ESLint configuration and integrate linting scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,37 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   typescript: {
-    typeCheck: true
+    typeCheck: true,
+  },
+  eslint: {
+    config: {
+      standalone: false,
+    },
   },
   css: ['@/assets/stylesheets/all.scss'],
+
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           additionalData: `@import "@/assets/stylesheets/variables";`,
           // silent sass deprecations
-          // https://github.com/twbs/bootstrap/issues/40962#issuecomment-2448214806 
+          // https://github.com/twbs/bootstrap/issues/40962#issuecomment-2448214806
           silenceDeprecations: ['mixed-decls', 'color-functions', 'global-builtin', 'import'],
           // https://stackoverflow.com/a/79003101
-          api: 'modern-compiler'
-        }
-      }
-    }
+          api: 'modern-compiler',
+        },
+      },
+    },
   },
+
   router: {
     options: {
-      linkActiveClass: "active",
-      linkExactActiveClass: "active",
+      linkActiveClass: 'active',
+      linkExactActiveClass: 'active',
     },
   },
 })
--- plugins/bootstrap.client.js
+++ plugins/bootstrap.client.js
@@ -1,5 +1,5 @@
-import bootstrap from "bootstrap/dist/js/bootstrap.bundle.min";
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.provide("bootstrap", bootstrap);
-});
+  nuxtApp.provide('bootstrap', bootstrap)
+})
