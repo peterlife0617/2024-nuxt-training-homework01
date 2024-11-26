df41d2ccf4e59af5d14b313b724b94d78bc7f023
dragon <peterlife0617@gmail.com>
2024-11-26 21:53:46 +0800

    refactor(sweetalert2): update plugin structure for better TypeScript support

--- plugins/sweetalert2.ts
+++ plugins/sweetalert2.ts
@@ -1,5 +1,11 @@
 import Swal from 'sweetalert2'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.provide('swal', Swal)
+export default defineNuxtPlugin(() => {
+  // 兩個寫法都可以，但第一種寫法 typescript 會沒有提示
+  // nuxtApp.provide('swal', Swal)
+  return {
+    provide: {
+      swal: Swal,
+    },
+  }
 })
