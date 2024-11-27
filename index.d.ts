ee9ac34d0cc22d75f4896ff9b9f95a62b35f15c9
dragon <peterlife0617@gmail.com>
2024-11-27 10:10:08 +0800

    feat(day18): implement loading overlay component and update index page

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,10 @@ declare module 'vue' {
     vTextformat: Directive<any, string, any, 'uppercase' | 'lowercase'>
     vTimeformat: Directive
   }
+
+  interface GlobalComponents {
+    Loading: typeof import ('vue-loading-overlay').Component
+  }
 }
 
 export { }
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,27 @@
 <script setup lang="ts">
-const message = ref('A1B2c3deFGhijk')
-const time = ref(1730427600000)
+const isLoading = ref(false)
+
+function openLoading() {
+  isLoading.value = true
+
+  // 開啟讀取效果
+  // ($loading as any).show()
+  setTimeout(() => {
+    // 關閉讀取效果
+    isLoading.value = false
+  }, 1000)
+}
 </script>
 
 <template>
-  <div class="container py-3">
-    <h2>自訂英文文字大小寫轉換指令</h2>
-    <!-- 使用 plugins/textformat.js 建立的指令，將變數 message 的字串帶入 -->
-
-    <!-- 大寫轉小寫格式之後，將結果寫入元素 -->
-    <p v-textformat:lowercase="message" />
-    <!-- 小寫轉大寫格式之後，將結果寫入元素 -->
-    <p v-textformat:uppercase="message" />
-
-    <h2>自訂時間轉換指令</h2>
-    <!-- 使用 plugins/timeformat.js 建立的指令，將變數 time 的時間戳帶入 -->
-    <!-- 轉換成 yyyy-mm-dd hh:mm:ss 格式之後將結果寫入元素 -->
-    <!-- 1730427600000 => 轉換成 yyyy-mm-dd hh:mm:ss  -->
-    <p v-timeformat="time" />
-  </div>
+  <button type="button" @click="openLoading">
+    開啟 Loading 效果
+  </button>
+  <ClientOnly>
+    <Loading
+      v-model:active="isLoading"
+    />
+  </ClientOnly>
 </template>
 
-<style scoped lang="scss">
-p {
-  font-size: 20px;
-}
-</style>
+<style scoped lang="scss"></style>
